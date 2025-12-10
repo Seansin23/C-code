4633effc4b131951af8b2268b57e1e0b8edf0d34
Seansin23 <seansin23@gmail.com>
2025-12-10 18:04:40 -0500

    Add convf_to_c.cpp file

---
+++ fahrenheit_celsius_convert/conv_f_to_c.cpp
@@ -0,0 +1,29 @@
+/* Purpose: Build a calculator that converts fahrenheit to celcius
+   Author: Sean Sinchi
+   Date: 11/16/2025*/
+#include <iostream>
+#include <cmath>
+int main(){
+  double tempf{};
+  double tempc{};
+  int x{};
+
+  std::cout << " Enter 1 to go from f->c or 2 to go from c->f" << std::endl;
+  std::cin >> x;
+   if(x==1)
+    {
+    std::cout << "Enter temperature in fahrenheit" << std::endl;
+    std::cin >> tempf;
+    tempc = (tempf - 32.0)*5.0 / 9.0;
+    std::cout << " Temperature in degrees celcius is: " << tempc << std::endl;
+    }
+  else if(x==2)
+    {
+      std::cout << "Enter temperature in celcius" << std::endl;
+    std::cin >> tempc;
+    tempf = (9.0/5.0 * tempc)+32.0;
+    std::cout << " Temperature in degrees fahrenheit is: " << tempf << std::endl;
+    }
+  return 0;
+
+}
